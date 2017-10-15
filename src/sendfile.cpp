f3079023186eb6f6c514f2ec94c0b2c13e4eaf70
aldrichvalentino <aldrich.vh97@gmail.com>
2017-10-15 13:11:37 +0700

    ubah dikit

--- src/sendfile.cpp
+++ src/sendfile.cpp
@@ -9,20 +9,13 @@
 
 using namespace std;
  
-#define SERVER "127.0.0.1"
-#define BUFLEN 256  //Max length of buffer
-#define PORT 8000   //The port on which to send data
+//#define SERVER "127.0.0.1"
+//#define BUFLEN 256  //Max length of buffer
+//#define PORT 8000   //The port on which to send data
  
-/* arguments: filename windowsize buffersize destination_ip destination_port*/
+/* arguments: filename windowsize buffersize destinationIp destinationPort*/
 int main(int argc, char ** argv)
 {
-    struct sockaddr_in si_other;
-    int s, i;
-    socklen_t slen = sizeof(si_other);
-    //char buf[BUFLEN];
-    char *buf = new char[BUFLEN];
-    char message[BUFLEN];
-
     char *filename;
     int windowSize;
     int buffersize;
@@ -38,13 +31,20 @@ int main(int argc, char ** argv)
         destinationIp = argv[4];
         destinationPort = atoi(argv[5]);
     }
- 
+
+    struct sockaddr_in si_other;
+    int s, i;
+    socklen_t slen = sizeof(si_other);
+    char *buf = new char[buffersize];
+    char message[buffersize];
+
+    /* Create Socket */
     s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     memset((char *) &si_other, 0, sizeof(si_other));
     si_other.sin_family = AF_INET;
-    si_other.sin_port = htons(PORT);
+    si_other.sin_port = htons(destinationPort);
      
-    if (inet_aton(SERVER , &si_other.sin_addr) == 0) 
+    if (inet_aton(destinationIp , &si_other.sin_addr) == 0) 
     {
         fprintf(stderr, "inet_aton() failed\n");
         exit(1);
@@ -60,12 +60,11 @@ int main(int argc, char ** argv)
          
         //receive a reply and print it
         //clear the buffer by filling null, it might have previously received data
-        for(int i = 0; i < BUFLEN; i++){
+        for(int i = 0; i < buffersize; i++){
             buf[i] = '\0';
         }
-        //memset(buf,'\0', BUFLEN);
         //try to receive some data, this is a blocking call
-        recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen);
+        recvfrom(s, buf, buffersize, 0, (struct sockaddr *) &si_other, &slen);
          
         puts(buf);
     }
